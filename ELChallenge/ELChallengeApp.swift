e00860daa0408e9c058bf8fb6d97d0ebcab1763f
Taina Saenz <tainamsaenz@gmail.com>
2023-08-14 19:26:23 -0700

    Initial Commit

---
+++ ELChallenge/ELChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ELChallengeApp.swift
+//  ELChallenge
+//
+//  Created by Taina Saenz on 8/14/23.
+//
+
+import SwiftUI
+
+@main
+struct ELChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
